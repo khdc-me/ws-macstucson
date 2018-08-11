a39b1cd2ffb2be9a5c87b5ee9c46631578dd1833
Kevin Harper <kevinharperg@gmail.com>
2018-08-11 16:21:43 -0700

    Create brake services pg

--- website/pages/urls.py
+++ website/pages/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('services/', views.services, name='services'),
     path('services/oil-change-packages/', views.oil_change_packages, name='oil-change-packages'),
+    path('services/brake-services/', views.brake_services, name='brake-services'),
     path('', views.home, name='home'),
 ]
