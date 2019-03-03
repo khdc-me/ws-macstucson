204af3dd260efcc0ef582bdf3d9166625cbc3f1c
Kevin <kevinharperg@gmail.com>
2019-03-03 16:20:26 -0700

    Removed paths from urls.py

--- website/pages/urls.py
+++ website/pages/urls.py
@@ -3,14 +3,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('services/', views.services, name='services'),
-    path('services/oil-change-packages/', views.oil_change_packages, name='oil-change-packages'),
-    path('services/brake-services/', views.brake_services, name='brake-services'),
-    path('services/suspension-services/', views.suspension_services, name='suspension-services'),
-    path('services/tires-wheels/', views.tires_wheels, name='tires-wheels'),
-    path('services/road-trip/', views.road_trip, name='road-trip'),
-    path('services/prepurchase-ins/', views.prepurchase_ins, name='prepurchase-ins'),
-    path('services/major-service/', views.major_service, name='major-service'),
-    path('privacy-policy/', views.privacy_policy, name='privacy-policy'),
     path('', views.home, name='home'),
-]
