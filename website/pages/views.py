2b4aa63df646d4719f6278490d178d756c2b95ad
Kevin <kevinharperg@gmail.com>
2019-03-03 16:27:15 -0700

    Updated check_is_open() in views.py

--- website/pages/views.py
+++ website/pages/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from datetime import datetime
-
 
 # Views
 def home(request):
@@ -64,12 +62,11 @@ def check_is_open():
     current_datetime = timezone.localtime(timezone.now())
     current_weekday = timezone.localtime(timezone.now()).strftime("%A")
     opens_at = current_datetime.replace(hour=7, minute=30)
-    weekday_closes_at = current_datetime.replace(hour=16, minute=30)
-    saturday_closes_at = current_datetime.replace(hour=13, minute=00)
+    weekday_closes_at = current_datetime.replace(hour=17, minute=00)
+    saturday_closes_at = current_datetime.replace(hour=13, minute=30)
     if ((current_weekday not in ['Saturday', 'Sunday'] and opens_at <= current_datetime < weekday_closes_at)
             or (current_weekday in ['Saturday'] and opens_at <= current_datetime < saturday_closes_at)):
         is_open = True
     else:
         is_open = False
-    return is_open
     return is_open
